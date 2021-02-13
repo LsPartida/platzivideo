d8fa58b2875d209d3461ee7904856257726c9047
Luis Partida <beluis1993@gmail.com>
2021-02-13 10:26:41 -0600

    Fase final del proyecto con fake API y React

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="footer">
+    <a href="/">Térmios de uso</a>
+    <a href="/">Declaración de privacidad</a>
+    <a href="/">Centro de ayuda</a>
+  </footer>
+);
+export default Footer;
